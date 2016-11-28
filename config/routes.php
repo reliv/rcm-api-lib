e0d686bfa41894d7f5c350698428fa6247e4279c
James Jervis <jjervis@relivinc.com>
2016-11-28 16:35:49 -0600

    Fix

--- config/routes.php
+++ config/routes.php
@@ -7,9 +7,7 @@ return [
         'name' => 'modules.rcm-api-lib.public',
         'path' => '/modules/rcm-api-lib/{fileName:.*}',
         'middleware' => \Reliv\RcmApiLib\Middleware\AssetController::class,
-        'options' => [
-            'test' => 'adsf',
-        ],
+        'options' => [],
         'allowed_methods' => ['GET'],
     ],
 ];

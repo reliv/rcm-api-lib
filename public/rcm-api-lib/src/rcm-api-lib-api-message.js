a26aa69940401524f98c8c0ea2b403118ee608e0
James Jervis <jjervis@relivinc.com>
2017-05-12 13:12:37 -0500

    Update for Promise BC

--- public/rcm-api-lib/src/rcm-api-lib-api-message.js
+++ public/rcm-api-lib/src/rcm-api-lib-api-message.js
@@ -1,5 +1,42 @@
 /**
- * Class ApiMessage
+ * {RcmApiLibServiceConfig}
+ * @param value
+ * @constructor
+ */
+var RcmApiLibServiceConfig = function (value) {
+    var self = this;
+    self.type = 'rcmApiLib';
+    self.source = 'client';
+    self.code = 'error';
+    self.value = rcmApiLibServiceConfig.defaultMessage;
+    self.primary = null;
+    self.params = [];
+    self.key = '';
+    // Client only property
+    self.level = 'warning';
+
+    /**
+     * buildKey
+     */
+    self.buildKey = function () {
+        self.key = self.type + '.' + self.source + '.' + self.code;
+    };
+
+    /**
+     * init
+     */
+    self.init = function (value) {
+        self.buildKey();
+        if (value) {
+            self.value = value;
+        }
+    };
+
+    self.init(value);
+};
+
+/**
+ * Class RcmApiLibServiceConfig
  */
 angular.module('rcmApiLib').factory(
     'rcmApiLibApiMessage',
@@ -8,39 +45,9 @@ angular.module('rcmApiLib').factory(
         function (rcmApiLibServiceConfig) {
 
             /**
-             * Class ApiMessage
+             * Class RcmApiLibServiceConfig
              */
-            return function (value) {
-                var self = this;
-                self.type = 'rcmApiLib';
-                self.source = 'client';
-                self.code = 'error';
-                self.value = rcmApiLibServiceConfig.defaultMessage;
-                self.primary = null;
-                self.params = [];
-                self.key = '';
-                // Client only property
-                self.level = 'warning';
-
-                /**
-                 * buildKey
-                 */
-                self.buildKey = function () {
-                    self.key = self.type + '.' + self.source + '.' + self.code;
-                };
-
-                /**
-                 * init
-                 */
-                self.init = function (value) {
-                    self.buildKey();
-                    if (value) {
-                        self.value = value;
-                    }
-                };
-
-                self.init(value);
-            };
+            return RcmApiLibServiceConfig;
         }
     ]
 );
--- public/rcm-api-lib/src/rcm-api-lib-service-config.js
+++ public/rcm-api-lib/src/rcm-api-lib-service-config.js
@@ -1,9 +1,15 @@
 /**
+ * {rcmApiLibServiceConfig}
+ * @type {{defaultMessage: string}}
+ */
+var rcmApiLibServiceConfig = {
+    defaultMessage: 'An unknown error occurred while making request'
+};
+
+/**
  * rcmApiLib Module
  */
 angular.module('rcmApiLib').value(
     'rcmApiLibServiceConfig',
-    {
-        defaultMessage: 'An unknown error occurred while making request'
-    }
+    rcmApiLibServiceConfig
 );
